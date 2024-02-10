ca8ade1e5230972712224a8558d57d8d712b0de9
IDGS-803-21000302 <21000302@alumnos.utleon.edu.mx>
2024-02-09 18:15:30 -0600

    Resistencias HTS 09/02/2024

--- main.py
+++ main.py
@@ -69,6 +69,33 @@ def num():
     return render_template("formularioDistancia.html",form=numForm,total=totalFinal)
 
 
+@app.route("/operaciones")
+def formulario1():
+    return render_template("formulario1.html")
+
+
+@app.route("/resultado",methods=["GET","POST"])
+def resultado():
+    if request.method=="POST":
+        num1 = request.form.get("n1")
+        num2 = request.form.get("n2")
+        operacion = request.form.get("operacion")
+
+        if operacion == "mult":
+            return "<h1>La multiplicacion es: {}<h1> ".format(str(int(num1)*int(num2)))
+    
+        elif operacion == "suma":
+            return "<h1>La suma es: {}<h1> ".format(str(int(num1)+int(num2)))
+    
+        elif operacion == "resta":
+            return "<h1>La diferencia es: {}<h1> ".format(str(int(num1)-int(num2)))
+    
+        elif operacion == "div":
+            return "<h1>La divicion es: {}<h1> ".format(str(int(num1)/int(num2)))
+    
+        
+
+
 
 @app.route("/resistencias",methods=["GET","POST"])
 def resistencias():
